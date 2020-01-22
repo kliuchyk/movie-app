1bc9db146309e2bb0295cacc47b7ca82f166f3e5
kliuchyk <kliuchyk@gmail.com>
2020-01-22 15:29:57 +0200

    add app header

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import React from 'react';
 import MoviesProvider from './providers/MovieProvider';
 import FavoritesProvider from './providers/FavoritesProvider';
 import MovieList from './containers/MovieList';
+import { PageHeader } from 'antd';
 
 function App() {
   return (
     <MoviesProvider>
       <FavoritesProvider>
+        <PageHeader className="page-header" title="MovieApp" />
         <div className="content">
           <MovieList />
         </div>
